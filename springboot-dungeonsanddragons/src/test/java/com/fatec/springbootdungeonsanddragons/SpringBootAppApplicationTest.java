5d2276742553fa08b1a02ea256d177cf3cec6857
Vitor Henrique da Silva <vhsilva@fitec.org.br>
2021-05-16 21:24:28 -0300

    test

--- springboot-dungeonsanddragons/src/test/java/com/fatec/springbootdungeonsanddragons/SpringBootAppApplicationTest.java
+++ springboot-dungeonsanddragons/src/test/java/com/fatec/springbootdungeonsanddragons/SpringBootAppApplicationTest.java
@@ -1,4 +1,4 @@
-package com.fatec.springbootdungeonsanddragons;
+package com.fatec.springbootdungeonsanddragons.entity;
 
 import java.util.List;
 import java.util.HashSet;
@@ -9,45 +9,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.fatec.springbootdungeonsanddragons.entity.Feiticeiro;
 import com.fatec.springbootdungeonsanddragons.entity.Usuario;
 import com.fatec.springbootdungeonsanddragons.entity.Personagem;
-import com.fatec.springbootdungeonsanddragons.entity.Grupo;
-import com.fatec.springbootdungeonsanddragons.entity.Lutador;
-import com.fatec.springbootdungeonsanddragons.entity.Admin;
-import com.fatec.springbootdungeonsanddragons.service.FeiticeiroService.java;
-import com.fatec.springbootdungeonsanddragons.service.GrupoService.java;
-import com.fatec.springbootdungeonsanddragons.service.UsuarioService.java;
-import com.fatec.springbootdungeonsanddragons.service.LutadorService.java;
-import com.fatec.springbootdungeonsanddragons.repository.FeiticeiroRepository;
-import com.fatec.springbootdungeonsanddragons.repository.GrupoRepository;
-import com.fatec.springbootdungeonsanddragons.repository.LutadorRepository;
 import com.fatec.springbootdungeonsanddragons.repository.UsuarioRepository;
 
 @SpringBootTest
 @Transactional
+@Rollback
 class SpringBootAppApplicationTests {
 
     @Autowired
     private UsuarioRepository usuarioRepo;
-    
-    @Autowired
-    private AutorizacaoRepository autRepo;
-
-    @Autowired
-    private PersonagemRepository personagemRepo;
-
-    @Autowired
-    private ClasseRepository classeRepo;
-
-    @Autowired
-    private SegurancaService segService;
-
-    @Autowired
-    private NovoPersonagemService perService;
 
     // TESTES DE USUARIO
-	@Test
+    @Test
 	void contextLoads() {
     }
     @Test
@@ -55,24 +30,6 @@ class SpringBootAppApplicationTests {
         Usuario usuario = new Usuario();
         usuario.setNomeUsuario("usuario_teste");
         usuario.setSenha("senha");
-        usuario.setAutorizacoes(new HashSet<Autorizacao>());
-            Autorizacao aut = new Autorizacao();
-            aut.setTipo("ROLE_USER");
-            autRepo.save(aut);
-            usuario.getAutorizacoes().add(aut);
         usuario.setNomeExibicao("teste");
-
-        usuarioRepo.save(usuario);
-        assertNotNull(usuario.getId());
-    }
-    // TESTES DE PERSONAGEM
-    @Test
-    void testaPersonagem() {
-        Personagem personagem = new Personagem();
-            personagem.setProprietario(usuarioRepo.findUsuarioByNomeUsuario("rodrigocr16"));
-            personagem.setClasse(classeRepo.findClasseByNome("bardo"));
-            personagem.setNome("seeker");
-        personagemRepo.save(personagem);
-        assertNotNull(personagem.getId());
     }
 }
